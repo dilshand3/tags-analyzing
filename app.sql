9522b7241c839cf35a4a872a20536a3f4a061c7f
dilshand3 <dilshan72404@gmail.com>
2026-06-05 15:00:00 +0530

    chore : remove redundent gin index

--- app.sql
+++ app.sql
@@ -1,4 +1,3 @@
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
@@ -11,13 +10,5 @@ CREATE TABLE IF NOT EXISTS documents (
 );
 
 CREATE INDEX IF NOT EXISTS idx_documents_tags_gin
-    ON documents
-    USING GIN((tags::text) gin_trgm_ops);;
-
-CREATE INDEX IF NOT EXISTS idx_documents_title_trgm
-    ON documents
-    USING GIN (title gin_trgm_ops);
-
-CREATE INDEX IF NOT EXISTS idx_documents_description_trgm
-    ON documents
-    USING GIN (description gin_trgm_ops);
+ON documents
+USING GIN(tags);
