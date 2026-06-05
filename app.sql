08f83abff3d84b2e29dee17d3e0907865082acce
dilshand3 <dilshan72404@gmail.com>
2026-06-05 16:00:00 +0530

    chore : add seprate table for tags

--- app.sql
+++ app.sql
@@ -18,3 +18,34 @@ DROP INDEX IF EXISTS idx_documents_tags_gin;
 CREATE INDEX IF NOT EXISTS idx_documents_tags_gin
 ON documents
 USING GIN(tags jsonb_path_ops);
+
+
+-- seprate tags table method
+
+CREATE TABLE IF NOT EXISTS short_videos (
+    id           UUID    PRIMARY KEY DEFAULT gen_random_uuid(),
+    title        TEXT    NOT NULL,
+    description  TEXT    NOT NULL,
+    created_at   DATE    NOT NULL DEFAULT CURRENT_DATE
+);
+
+
+CREATE TABLE IF NOT EXISTS tags (
+    id          UUID    PRIMARY KEY DEFAULT gen_random_uuid(),
+    name        TEXT    NOT NULL,
+    slug        TEXT    NOT NULL,
+    tag_type    TEXT    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS content_tags (
+    content_id  UUID NOT NULL REFERENCES short_videos(id) ON DELETE CASCADE,
+    tag_id      UUID NOT NULL REFERENCES tags(id) ON DELETE CASCADE,
+    PRIMARY KEY (content_id, tag_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_tags_name_trgm
+ON tags
+USING GIN(name gin_trgm_ops);
+
+CREATE INDEX IF NOT EXISTS idx_content_tags_tag_id
+ON content_tags (tag_id, content_id);
